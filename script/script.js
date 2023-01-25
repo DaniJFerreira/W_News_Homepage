edd1fbbc7b9a3ed01637287f29f22ac12592c0c4
Daniel Ferreira <daniel_jferreira@hotmail.com>
2023-01-25 11:27:02 +0000

    add jquery code to create hover effects on navbar, h5 and h6 titles sections

--- script/script.js
+++ script/script.js
@@ -1,10 +1,48 @@
-let navbarLinks = document.querySelectorAll(".navbar a");
+//for navbar hover color animation 
+$(document).ready(function(){
+  $('.navbar ul li a').hover(function(){
+    $(this).css('color', 'hsl(5, 85%, 63%)');
+    $(this).find('after').css({
+      'width': '100%',
+      'z-index': '-10',
+      'animation':  '1s forwards',
+      '-webkit-animation': 'fill 1s forwards',
+      '-moz-animation':'fill 1s forwards',
+      'opacity':'1'
+    });
 
-navbarLinks.forEach(link => {
-  link.addEventListener("mouseenter", function(event) {
-    event.target.style.color = "red";
+  }, function(){
+  $(this).css('color', '');  //reset color to default
+  $(this).find('after').css({  //reset css properties to default values 
+      'width':'0%',  
+      'z-index':'1',  
+      'animation':'none',  
+      '-webkit-animation':'none',  
+      '-moz-animation':'none',  
+      'opacity':'0'  
   });
-  link.addEventListener("mouseleave", function(event) {
-    event.target.style.color = "black";
+//for navbar underline animation effect
+  },function(){
+    $('#navbar').animate({
+      width: '0%',
+      height: '1px'
+    }, 0);
+  
+    $('#navbar').animate({
+      width: '100%',
+      height: '1px'
+    }, 500);
+  
+    $('#navbar').animate({
+      width: '100%',
+      height: '100%',
+      backgroundColor: '#333'
+    }, 1000);  
+
+  }, function(){
+    $('h5 a','h6 a').hover({
+      'color': 'hsl(5, 85%, 63%)'
+    });
+
   });
-});
+}); 
