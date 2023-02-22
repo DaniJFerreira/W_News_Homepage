895e778941591c2557928ad72de5f499520d11d1
Daniel Ferreira <daniel_jferreira@hotmail.com>
2023-02-22 13:07:37 +0000

    font size update in order to increase by one

--- script/script.js
+++ script/script.js
@@ -1,48 +1,66 @@
-//for navbar hover color animation 
-$(document).ready(function(){
-  $('.navbar ul li a').hover(function(){
-    $(this).css('color', 'hsl(5, 85%, 63%)');
-    $(this).find('after').css({
-      'width': '100%',
-      'z-index': '-10',
-      'animation':  '1s forwards',
-      '-webkit-animation': 'fill 1s forwards',
-      '-moz-animation':'fill 1s forwards',
-      'opacity':'1'
-    });
+// $(document).ready(function(){
+//   $('h5 a').hover(function(){
+//   $(this).css('color', '#F55050');
+// }, function(){
+//   $(this).css('color', '#FFFF');
+// });
 
-  }, function(){
-  $(this).css('color', '');  //reset color to default
-  $(this).find('after').css({  //reset css properties to default values 
-      'width':'0%',  
-      'z-index':'1',  
-      'animation':'none',  
-      '-webkit-animation':'none',  
-      '-moz-animation':'none',  
-      'opacity':'0'  
-  });
-//for navbar underline animation effect
-  },function(){
-    $('#navbar').animate({
-      width: '0%',
-      height: '1px'
-    }, 0);
+
+// // hover effect
+// $('h6 a').hover(function(){
+//   $(this).css('color', '#F55050');
+// }, function(){
+//   $(this).css('color', 'hsl(240, 100%, 5%');
+// });
+
+
+
+// });
+// //for navbar hover color animation 
+// $(document).ready(function(){
+//   $('.navbar ul li a').hover(function(){
+//     $(this).css('color', 'hsl(5, 85%, 63%)');
+//     $(this).find('after').css({
+//       'width': '100%',
+//       'z-index': '-10',
+//       'animation':  '1s forwards',
+//       '-webkit-animation': 'fill 1s forwards',
+//       '-moz-animation':'fill 1s forwards',
+//       'opacity':'1'
+//     });
+
+//   }, function(){
+//   $(this).css('color', '');  //reset color to default
+//   $(this).find('after').css({  //reset css properties to default values 
+//       'width':'0%',  
+//       'z-index':'1',  
+//       'animation':'none',  
+//       '-webkit-animation':'none',  
+//       '-moz-animation':'none',  
+//       'opacity':'0'  
+//   });
+// //for navbar underline animation effect
+//   },function(){
+//     $('#navbar').animate({
+//       width: '0%',
+//       height: '1px'
+//     }, 0);
   
-    $('#navbar').animate({
-      width: '100%',
-      height: '1px'
-    }, 500);
+//     $('#navbar').animate({
+//       width: '100%',
+//       height: '1px'
+//     }, 500);
   
-    $('#navbar').animate({
-      width: '100%',
-      height: '100%',
-      backgroundColor: '#333'
-    }, 1000);  
+//     $('#navbar').animate({
+//       width: '100%',
+//       height: '100%',
+//       backgroundColor: '#333'
+//     }, 1000);  
 
-  }, function(){
-    $('h5 a','h6 a').hover({
-      'color': 'hsl(5, 85%, 63%)'
-    });
+//   }, function(){
+//     $('h5 a').hover({
+//       'color': '#F55050'
+//     });
 
-  });
-}); 
+//   });
+// }); 
